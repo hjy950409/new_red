538aa16b62e3c56261161f5a42cf253c7f46fe61
hjy950409 <hjy950409@naver.com>
2021-07-16 16:54:30 +0900

    최종수정 한번 해봄

---
+++ docs/common.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+  // And if we need scrollbar
